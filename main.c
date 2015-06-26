c79b0ce0ae16ca32cd898f17de3a2b27da909c58
Vladimir <v-for-vendeta@hotmail.rs>
2015-06-26 21:48:52 +0200

    Added function "createDatabaseIfNotExists", to check if database exists if not then created database.

--- main.c
+++ main.c
@@ -8,11 +8,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TRUE  1
+#define FALSE 0
+
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define RESET "\033[0m"
+
+char createDatabaseIfNotExists();
 /*
  * 
  */
 int main(int argc, char** argv) {
     
+    if(createDatabaseIfNotExists("database")){
+        printf(KGRN "[+] " RESET "DATABSE FILE EXISTS\n");
+        puts(KGRN "[+] " RESET "INITIALIZATION STARTED...\n");
+        
+    }else{
+        printf(KRED "[-] " RESET "DATABASE FILE NOT EXISTS");
+    }
     return (EXIT_SUCCESS);
 }
 
+char createDatabaseIfNotExists(char * filename){
+    FILE * database;
+    if(database = fopen(filename, "r")){
+        fclose(database);
+        return TRUE;
+    }else{
+        database = fopen(filename, "w");
+        fprintf(database, "TEST");
+        fclose(database);
+        puts(KGRN "[+] " RESET "DATABASE FILE CREATED\n");
+        puts(KGRN "[+] " RESET "INITIALIZATION STARTED...\n");
+        return FALSE;
+    }
+}
